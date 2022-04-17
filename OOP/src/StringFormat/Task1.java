da029525018211b5285517da83810c8903a71f97
Hihukas <aleks.dudenas@gmail.com>
2022-04-17 12:39:25 +0300

    StringFormat Task

---
+++ OOP/src/StringFormat/Task1.java
@@ -0,0 +1,14 @@
+package StringFormat;
+
+public class Task1 {
+    public static void main(String[] args) {
+       String word1 = "Aš";
+        String word2 = "tikrai";
+        String word3 = "išmoksiu";
+        String word4 = "programuoti";
+        String word5 = "Java";
+        System.out.printf("|%20s|\n|%20s|\n|%20s|\n|%20s|\n|%-20s|\n",word1, word2, word3, word4, word5);
+
+
+    }
+}
