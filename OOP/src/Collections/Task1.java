04d7a7a340bb49e78334739030fcd20834cf06bf
Hihukas <aleks.dudenas@gmail.com>
2022-04-17 14:36:49 +0300

    Collection task1

---
+++ OOP/src/Collections/Task1.java
@@ -0,0 +1,15 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1990);
+        list.add(6);
+        list.add(15);
+        System.out.println(list.get(1));
+    }
+
+}
