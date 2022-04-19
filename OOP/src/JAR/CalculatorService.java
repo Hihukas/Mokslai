6b749026b35a0c6eb98aa01e77f5c8eac8a80a3e
Hihukas <aleks.dudenas@gmail.com>
2022-04-19 13:38:27 +0300

    JAR calculator task

--- OOP/src/JAR/CalculatorService.java
+++ OOP/src/JAR/CalculatorService.java
@@ -4,4 +4,16 @@ public class CalculatorService {
     public int sum(int a, int b) {
         return a + b;
     }
+
+    public int minus(int a, int b){
+        return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
+    }
 }
--- OOP/src/JAR/JarMain.java
+++ OOP/src/JAR/JarMain.java
@@ -1,17 +1,67 @@
 package JAR;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class JarMain {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        JarMain jarMain = new JarMain();
+        jarMain.menu();
+
+        String action = scanner.nextLine();
+        jarMain.selecAction(scanner, action, args);
+
+
+    }
+
+    private void menu() {
+        System.out.println("""
+                a. sum
+                b. minus
+                c. multiply
+                d. divide
+                """);
+    }
+
+    private void selecAction(Scanner scanner, String action, String[] args) {
         CalculatorService calculatorService = new CalculatorService();
 
-        if (args.length < 2) {
-            System.out.println("Nėra parametrų.");
-            return;
+        int a = getCorrectNumber(args.length > 0 ? args[0] : null, scanner);
+        int b = getCorrectNumber(args.length > 1 ? args[1] : null, scanner);
+
+        switch (action) {
+            case "a" -> System.out.printf("Suma yra: %s\n", calculatorService.sum(a, b));
+            case "b" -> System.out.printf("Skirtumas yra: %s\n", calculatorService.minus(a, b));
+            case "c" -> System.out.printf("Daugybos rezultatas yra: %s\n", calculatorService.multiply(a, b));
+            case "d" -> {
+                try {
+                    int result = calculatorService.divide(a, b);
+                    System.out.printf("Dalybos rezultatas yra: %s\n", result);
+                } catch (ArithmeticException e) {
+                    System.out.println("Dalyba iš nulio negalima.");
+                }
+            }
+            default -> System.out.println("Nėra tokio veiksmo.");
         }
 
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
+    }
 
-        System.out.printf("Suma yra: %s\n", calculatorService.sum(a, b));
+    private int getCorrectNumber(String value, Scanner scanner) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+        }
+
+        while (true) {
+            try {
+                System.out.println("Įveskite skaičių:");
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Blogas formatas.");
+                scanner.nextLine();
+            }
+        }
     }
 }
