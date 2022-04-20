726ae3453d8a2a40bc26c94b4086236d88fac813
Hihukas <aleks.dudenas@gmail.com>
2022-04-20 19:28:02 +0300

    Maven submodules examples

---
+++ Maven/Maven_Parent_Example/Child1/src/main/java/lt/codeacademy/child/SimpleService.java
@@ -0,0 +1,7 @@
+package lt.codeacademy.child;
+
+public class SimpleService {
+    public void sayHello(){
+        System.out.println("labas");
+    }
+}
---
+++ Maven/Maven_Parent_Example/Child2/src/main/java/lt/codeacademy/child/Main.java
@@ -0,0 +1,9 @@
+package lt.codeacademy.child;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleService simpleService = new SimpleService();
+        simpleService.sayHello();
+
+    }
+}
