2f24d518dd0e636c9a4703e4b5f9ebb2f0e8952a
Hihukas <aleks.dudenas@gmail.com>
2022-04-20 13:57:31 +0300

    Map task2

--- OOP/src/Collections/Task9.java
+++ OOP/src/Collections/Task9.java
@@ -9,9 +9,25 @@ public class Task9 {
         map.put(11, "Vienuolika");
         map.put(12, "Dvylika");
         map.put(100, "Šimtas");
+
         System.out.println(map);
-        System.out.printf("%s - %s\n", map.containsKey(12), map.containsValue("Dvylika"));
+
+        if(map.containsKey(11) && map.containsValue("Vienuolika")){
+            System.out.println("Yra tokia pora.");
+        }
+
         System.out.println(map.get(100));
+
+        Task9 task9 = new Task9();
+        task9.printMap(map);
+
+        map.remove(11);
+        task9.printMap(map);
+        map.clear();
+        task9.printMap(map);
+    }
+
+    private void printMap(Map<Integer, String> map) {
         for (Integer key : map.keySet()) {
             System.out.printf("Raktas: %s, Reikšmė: %s\n", key, map.get(key));
         }
