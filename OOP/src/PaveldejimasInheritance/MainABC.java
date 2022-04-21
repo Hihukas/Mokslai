7d2c72eb951ef6157fbacab2d5c630c0e58a8d58
Hihukas <aleks.dudenas@gmail.com>
2022-04-21 13:16:58 +0300

    Paveldėjimo pavyzdys instance of pavyzdys

--- OOP/src/PaveldejimasInheritance/MainABC.java
+++ OOP/src/PaveldejimasInheritance/MainABC.java
@@ -3,5 +3,9 @@ package PaveldejimasInheritance;
 public class MainABC {
     public static void main(String[] args) {
         C c = new C();
+        B b = new B();
+        System.out.println(c instanceof C);
+        System.out.println(c instanceof B);
+        System.out.println(b instanceof C);
     }
 }
