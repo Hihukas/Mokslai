79c16b126deb2e358ce7d4560df5223e7c99539b
Hihukas <aleks.dudenas@gmail.com>
2022-04-25 11:36:06 +0300

    Json įrašymas, nuskaitymas, pvz.

--- Maven/json_example/src/main/java/lt/codeacademy/json/example/Json/MainJsonExample.java
+++ Maven/json_example/src/main/java/lt/codeacademy/json/example/Json/MainJsonExample.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class MainJsonExample {
     public static void main(String[] args) throws IOException {
@@ -13,14 +14,24 @@ public class MainJsonExample {
         mapper.enable(SerializationFeature.INDENT_OUTPUT); // Suformatuoja į Json kalbą
 
         File file = new File("persons.json");
-        if(!file.exists()){
+        if (!file.exists()) {
             file.createNewFile();
         }
 
-        Person person = new Person("Andrius", "Baltrūnas", 33, new Address("Kaunas", "LT", "Savanorių pr."));
+        Person person = new Person("Andrius", "Baltrūnas", 33, new Address("Vilnius", "LT", "Savanorių pr."));
+        Person secondPerson = new Person("Aleksandras", "Dudėnas", 31, new Address("Klaipėda", "LT", "Turgaus a."));
         mapper.writeValue(file, person);
 
         person = mapper.readValue(file, Person.class);
         System.out.println(person);
+
+        String personAssString = mapper.writeValueAsString(person);
+        System.out.println(personAssString);
+
+        person = mapper.readValue(personAssString, Person.class);
+        System.out.println(person);
+
+        List<Person> persons = List.of(person, secondPerson, person);
+        mapper.writeValue(file, persons);
     }
 }
