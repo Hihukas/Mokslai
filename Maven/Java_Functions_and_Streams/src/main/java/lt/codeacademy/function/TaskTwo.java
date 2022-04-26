eac3f1c1fdc9a1aaefc51113f776fd767d13a38b
Hihukas <aleks.dudenas@gmail.com>
2022-04-26 21:35:20 +0300

    Task two functions

---
+++ Maven/Java_Functions_and_Streams/src/main/java/lt/codeacademy/function/TaskTwo.java
@@ -0,0 +1,45 @@
+package lt.codeacademy.function;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TaskTwo {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Andrius");
+        names.add("Jonas");
+        names.add("Petras");
+        names.add("Jokubas");
+        names.add("Jonas");
+        TaskTwo taskTwo = new TaskTwo();
+        taskTwo.methodOne(taskTwo.sortNames(), names);
+        taskTwo.methodOne(taskTwo.upperCaseNames(), names);
+    }
+
+    private void methodOne(Function<List<String>, List<String>> function, List<String> names) {
+        names = function.apply(names);
+        names.forEach(System.out::println);
+    }
+
+    private Function<List<String>, List<String>> sortNames() {
+        return names -> {
+            Collections.sort(names);
+            return names;
+        };
+    }
+
+    private Function<List<String>, List<String>> upperCaseNames() {
+       /*  return names -> {
+         List<String> newArray = new ArrayList<>();
+          for(String name: names){
+              newArray.add(name.toUpperCase())
+          }
+          return newArray;
+        };*/
+        return names -> names.stream().map(String::toUpperCase).collect(Collectors.toList());
+    }
+}
