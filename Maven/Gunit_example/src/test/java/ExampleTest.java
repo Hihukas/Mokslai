2c888dc47b9059a6107ab83a61811827d031ba29
Hihukas <aleks.dudenas@gmail.com>
2022-05-03 11:13:23 +0300

    test cases for example class

--- Maven/Gunit_example/src/test/java/ExampleTest.java
+++ Maven/Gunit_example/src/test/java/ExampleTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,24 +13,49 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
-//@ExtendWith({MockitoExtension.class})
+@ExtendWith({MockitoExtension.class})
 public class ExampleTest {
 
-    //@InjectMocks
+    @InjectMocks
     private Example example;
 
-   // @Mock
-    //private UserService userService;
+   @Mock
+    private UserService userService;
     private UUID id;
 
     @BeforeEach
     void setUp(){
        // MockitoAnnotations.openMocks(this);
-        UserService userService = Mockito.mock(UserService.class);
-        example = new Example(userService);
+        //UserService userService = Mockito.mock(UserService.class);
+        //example = new Example(userService);
 
         id = UUID.randomUUID();
     }
+
+    @Test
+    void testCreateNewUserWhenUserServiceThrowException(){
+        doThrow(IllegalArgumentException.class).when(userService).getUsername();
+
+        assertThrows(IllegalArgumentException.class, () -> example.createrNewUser(null));
+    }
+
+    @Test
+    void testCreateNewUser(){
+        when(userService.getUsername()).thenReturn("DummyName");
+
+        boolean isUserCreated = example.createrNewUser(new User(12));
+
+        assertTrue(isUserCreated);
+    }
+
+    @Test
+    void testCreateNewUserWhenUserNameIsNull(){
+        when(userService.getUsername()).thenReturn(null);
+        boolean isUserCreated = example.createrNewUser(new User(11));
+
+        assertFalse(isUserCreated);
+    }
+
 
     @Test
     void testCreaterNewUserWhenUserAgeIsLess(){
