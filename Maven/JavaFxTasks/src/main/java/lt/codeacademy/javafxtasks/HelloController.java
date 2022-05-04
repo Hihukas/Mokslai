dc6f8171dcf3f6815d29b6332ede7d9f66fdcd75
Hihukas <aleks.dudenas@gmail.com>
2022-05-04 20:22:32 +0300

    Java Fx task one

--- Maven/JavaFxTasks/src/main/java/lt/codeacademy/javafxtasks/HelloController.java
+++ Maven/JavaFxTasks/src/main/java/lt/codeacademy/javafxtasks/HelloController.java
@@ -1,14 +1,34 @@
 package lt.codeacademy.javafxtasks;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField nameField;
+
+    @FXML
+    private TextField surnameField;
+
+    @FXML
+    private Label helloLabel;
+
+    @FXML
+    protected void sayHello(){
+        String name = nameField.getText();
+        String surname = surnameField.getText();
+        if(name == null || name.equals("") || surname == null || surname.equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Laukeliai negali būti tušti!");
+            alert.show();
+        } else{
+            nameField.setText("");
+            surnameField.setText("");
+
+            helloLabel.setText("Sveiki " + name + " " + surname);
+        }
     }
 }
