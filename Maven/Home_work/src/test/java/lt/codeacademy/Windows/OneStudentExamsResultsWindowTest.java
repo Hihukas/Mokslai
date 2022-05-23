00e815860d75ae2f033cd472fd18f650eba312b0
Hihukas <aleks.dudenas@gmail.com>
2022-05-23 15:43:33 +0300

    Home work

--- Maven/Home_work/src/test/java/lt/codeacademy/Windows/OneStudentExamsResultsWindowTest.java
+++ Maven/Home_work/src/test/java/lt/codeacademy/Windows/OneStudentExamsResultsWindowTest.java
@@ -1,43 +1,21 @@
 package lt.codeacademy.Windows;
 
-import lt.codeacademy.Answers.StudentsAnswers;
 import lt.codeacademy.MainModel;
-import org.junit.jupiter.api.Assertions;
+import lt.codeacademy.Users.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @ExtendWith({MockitoExtension.class})
 class OneStudentExamsResultsWindowTest {
 
-    @InjectMocks
-    private  OneStudentExamsResultsWindow oneStudentExamsResultsWindow;
-
     @Mock
     private MainModel mainModel;
+    private User user;
 
     @Test
-    void testFillOneStudentsAnswersListWhenStudentsAnswersListIsEmpty() {
-        List<StudentsAnswers> filledOneStudentsAnswersList = new ArrayList<>();
-
-        for (StudentsAnswers a : mainModel.getStudentsAnswersList()) {
-            if (a.getUser().getId().equals(mainModel.getUser().getId())) {
-                filledOneStudentsAnswersList.add(a);
-            }
-        }
-
-        Assertions.assertEquals(0, filledOneStudentsAnswersList.size());
+    void test() {
     }
-
-//    @Test
-//    void testFillOneStudentsAnswersListWhenStudentsAnswersListHasOtherStudentsAnswers() {
-//        List<StudentsAnswers> filledOneStudentsAnswersList = new ArrayList<>();
-//        Mockito.when(mainModel.getStudentsAnswersList().size()).thenReturn(1);
-//        Assertions.assertEquals(1, filledOneStudentsAnswersList.size());
-//    }
 }
+
