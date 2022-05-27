57f62ccc3dc79317b2a0bed6af8c5c09a44dbb80
Hihukas <aleks.dudenas@gmail.com>
2022-05-27 14:14:57 +0300

    JDBC example postgres update, delete examples DB

--- DB/JDBC_example/src/main/java/lt/codeacademy/postgress/PostgresJdbcExample.java
+++ DB/JDBC_example/src/main/java/lt/codeacademy/postgress/PostgresJdbcExample.java
@@ -18,13 +18,25 @@ public class PostgresJdbcExample {
             example.printResult(resultSet);
 
             // insert example
-//            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO public.\"Bank\" values (?, ?, ?, ?)");
-//
-//            preparedStatement.setInt(1, 5);
-//            preparedStatement.setString(2, "Šiauliai bankas");
-//            preparedStatement.setString(3, "Š");
-//            preparedStatement.setInt(4, 5);
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO public.\"Bank\" values (?, ?, ?, ?)");
+
+            preparedStatement.setInt(1, 5);
+            preparedStatement.setString(2, "Šiauliai bankas");
+            preparedStatement.setString(3, "Š");
+            preparedStatement.setInt(4, 5);
 //            preparedStatement.execute();
+
+            // update example
+            preparedStatement = connection.prepareStatement("UPDATE public.\"Bank\" set\"name\"=? where \"id\"=?");
+            preparedStatement.setInt(2, 5);
+            preparedStatement.setString(1, "Šiauliai Bank");
+//            preparedStatement.execute();
+
+            // delete example
+            preparedStatement = connection.prepareStatement("Delete from public.\"Bank\" where \"id\" = ?");
+            preparedStatement.setInt(1, 5);
+            preparedStatement.execute();
+
         } catch (SQLException e) {
             System.out.println("SQL exception" + e.getMessage());
         }
