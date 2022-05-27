3c72c9fe487e8d27e7e58e4e2f46668e00a7b266
Hihukas <aleks.dudenas@gmail.com>
2022-05-27 13:53:33 +0300

    JDBC example postgres DB

---
+++ DB/JDBC_example/src/main/java/lt/codeacademy/postgress/PostgresJdbcExample.java
@@ -0,0 +1,29 @@
+package lt.codeacademy.postgress;
+
+import java.sql.*;
+
+public class PostgresJdbcExample {
+    private static final String URL = "jdbc:postgresql://127.0.0.1/postgres";
+    private static final String NAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static void main(String[] args) {
+        PostgresJdbcExample example = new PostgresJdbcExample();
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD);
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM public.\"Bank\"");
+            example.printResult(resultSet);
+        } catch (SQLException e) {
+            System.out.println("SQL exception" + e.getMessage());
+        }
+    }
+
+    private void printResult(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt("id") + " " + resultSet.getString("name") + " " + resultSet.getString("vat_code") + " " + resultSet.getInt("registration_code"));
+        }
+    }
+}
