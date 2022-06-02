9232bb636cd79200c99c4915dfa8e7712d7b021e
Hihukas <aleks.dudenas@gmail.com>
2022-06-02 14:04:12 +0300

    Hibernate example new ueser create

--- DB/HibernateExample/src/main/java/lt/codeacademy/Entity/User.java
+++ DB/HibernateExample/src/main/java/lt/codeacademy/Entity/User.java
@@ -1,5 +1,6 @@
 package lt.codeacademy.Entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    //    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY);
+
+    @Id
     private Long id;
     private String name;
     private String surname;
+    @Column(name = "e-mail", nullable = false)
     private String email;
 }
